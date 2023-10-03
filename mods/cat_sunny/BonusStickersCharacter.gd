e0db16044d18c7243bd231ab4c4727ece84e3715
Yukita Mayako <catgirl@goddess.moe>
2023-10-02 20:42:03 -0400

    Release v1.1
    * Change Sunny's starter to Pawndead
    * Change Sunny's signature species to Mascotoy
    * Remove outdated bugfix patches
    * Fix crash in Cassette Beasts v1.5 update

--- mods/cat_sunny/BonusStickersCharacter.gd
+++
@@ -1,5 +0,0 @@
-extends "res://mods/cat_sunny/BugfixCharacter.gd"
-
-
-# BattleMoves
-export(Array, Resource) var bonus_stickers: Array
--- mods/cat_sunny/mod.gd
+++ mods/cat_sunny/mod.gd
@@ -1,6 +1,10 @@
 extends ContentInfo
 
 
+const MODUTILS = {
+	"updates": "https://gist.githubusercontent.com/Yukitty/f113b1e2c11faad763a47ebc0a867643/raw/updates.json"
+}
+
 var CHARACTER_SUNNY: Character
 
 const WORLD_SCENE_CALLBACKS: Dictionary = {
@@ -11,13 +15,7 @@ const WORLD_SCENE_CALLBACKS: Dictionary = {
 # Script-modified Resources MUST remain in some
 # global var, or else the changes will be lost.
 # This includes take_over_path getting reverted.
-var swap_partner_cutscene: PackedScene
-var sunny_punk: PackedScene
-var camping: PackedScene
-var partner_archangel_reaction: PackedScene
-var partner_archangel_reaction_counter: PackedScene
-var miss_mimic_spawn_chest_interact: PackedScene
-var save_state_party: GDScript
+var res_cache: Array
 
 
 func init_content() -> void:
@@ -29,10 +27,6 @@ func init_content() -> void:
 	# World scene callbacks
 	SceneManager.connect("scene_changed", self, "_on_SceneManager_scene_changed")
 	SceneManager.connect("scene_change_starting", self, "_on_SceneManager_scene_changed", [], CONNECT_ONESHOT)
-
-	# Extend SaveState.party (bootleg bugfix and Bonus Stickers)
-	save_state_party = load("res://mods/cat_sunny/Party.gd")
-	save_state_party.take_over_path("res://global/save_state/Party.gd")
 
 	# Finish initialization later
 	assert(not SceneManager.preloader.singleton_setup_complete)
@@ -53,35 +47,42 @@ func init_content() -> void:
 	res.resource_path = "res://mods/cat_sunny/world/arms_cat_sunny.tscn"
 	_add_part_to_index(HumanLayersHelper.world_layer_table.arms, res)
 
-	# I have no idea what this does, I'm GRASPING here
-	if not "cat_sunny" in HumanLayersHelper.bodies:
-		HumanLayersHelper.bodies.push_back("cat_sunny")
-	assert("cat_sunny" in HumanLayersHelper.arms)
+	# I don't believe this is required. I don't know what it is used for.
+#	if not "cat_sunny" in HumanLayersHelper.bodies:
+#		HumanLayersHelper.bodies.push_back("cat_sunny")
+#	assert("cat_sunny" in HumanLayersHelper.arms)
 
 	# Late load Sunny's Character
 	CHARACTER_SUNNY = load("res://mods/cat_sunny/character_sunny.tres")
 
 	# Extend SunnyPunk
-	sunny_punk = load("res://mods/cat_sunny/SunnyPunk.tscn")
-	sunny_punk.take_over_path("res://world/recurring_npcs/SunnyPunk.tscn")
+	res = load("res://mods/cat_sunny/SunnyPunk.tscn")
+	res.take_over_path("res://world/recurring_npcs/SunnyPunk.tscn")
+	res_cache.push_back(res)
 
 	# Extend SwapPartnerCutscene
-	swap_partner_cutscene = load("res://mods/cat_sunny/cutscenes/SwapPartnerCutscene.tscn")
-	swap_partner_cutscene.take_over_path("res://nodes/partners/SwapPartnerCutscene.tscn")
+	res = load("res://mods/cat_sunny/cutscenes/SwapPartnerCutscene.tscn")
+	res.take_over_path("res://nodes/partners/SwapPartnerCutscene.tscn")
+	res_cache.push_back(res)
 
 	# Extend Camping cutscene
-	camping = load("res://mods/cat_sunny/cutscenes/Camping.tscn")
-	camping.take_over_path("res://cutscenes/Camping.tscn")
+	res = load("res://mods/cat_sunny/cutscenes/Camping.tscn")
+	res.take_over_path("res://cutscenes/Camping.tscn")
+	res_cache.push_back(res)
 
 	# Extend PartnerArchangelReaction
-	partner_archangel_reaction = load("res://mods/cat_sunny/cutscenes/PartnerArchangelReaction.tscn")
-	partner_archangel_reaction.take_over_path("res://cutscenes/archangels/PartnerArchangelReaction.tscn")
-	partner_archangel_reaction_counter = load("res://mods/cat_sunny/cutscenes/PartnerArchangelReaction_CountInc.tscn")
-	partner_archangel_reaction_counter.take_over_path("res://cutscenes/archangels/PartnerArchangelReaction_CountInc.tscn")
+	res = load("res://mods/cat_sunny/cutscenes/PartnerArchangelReaction.tscn")
+	res.take_over_path("res://cutscenes/archangels/PartnerArchangelReaction.tscn")
+	res_cache.push_back(res)
+
+	res = load("res://mods/cat_sunny/cutscenes/PartnerArchangelReaction_CountInc.tscn")
+	res.take_over_path("res://cutscenes/archangels/PartnerArchangelReaction_CountInc.tscn")
+	res_cache.push_back(res)
 
 	# Extend MissMimicSpawn_ChestInteract
-	miss_mimic_spawn_chest_interact = load("res://mods/cat_sunny/cutscenes/MissMimicSpawn_ChestInteract.tscn")
-	miss_mimic_spawn_chest_interact.take_over_path("res://cutscenes/passive_quests/MissMimicSpawn_ChestInteract.tscn")
+	res = load("res://mods/cat_sunny/cutscenes/MissMimicSpawn_ChestInteract.tscn")
+	res.take_over_path("res://cutscenes/passive_quests/MissMimicSpawn_ChestInteract.tscn")
+	res_cache.push_back(res)
 
 	# Add sunny to the global partners list
 	SaveState.party.source_partners.push_back(CHARACTER_SUNNY)
@@ -97,11 +98,13 @@ func init_content() -> void:
 
 func _add_part_to_index(index: LayerPartIndex, part: PackedScene) -> void:
 	index.parts.push_back(part)
+	# If _analyze hasn't been called yet, allow it to do the work
 	if index._parts_by_name.empty():
 		return
-	var pn: String = index.get_part_name(part)
-	index._parts_by_name[pn] = part
-	index.part_names.push_back(pn)
+	# Post-analyze, add part names ourself.
+	var name: String = index.get_part_name(part)
+	index._parts_by_name[name] = part
+	index.part_names.push_back(name)
 
 
 func _on_SceneManager_scene_changed() -> void:
